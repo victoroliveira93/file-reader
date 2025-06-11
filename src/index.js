a0923705781091588826732d7bb87b0a574b22d3
Victor <victor_oliveira93@outlook.com>
2025-06-11 18:18:19 -0300

    lidando com erros

--- src/index.js
+++ src/index.js
@@ -4,5 +4,44 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, "utf-8", (erro, texto) => {
-  console.log(texto);
+  try {
+    if (erro) throw erro;
+    contaPalavras(texto);
+  } catch (erro) {
+    if (erro.code === "ENOENT") console.log("erro que esperava");
+    else console.log("outro erro");
+  }
 });
+
+function contaPalavras(texto) {
+  const paragrafos = extraiParagrafos(texto);
+  const contagem = paragrafos.flatMap((paragrafo) => {
+    if (!paragrafo) return [];
+    return verificaPalavrasDuplicadas(paragrafo);
+  });
+  console.log(contagem);
+}
+
+function extraiParagrafos(texto) {
+  return texto.toLowerCase().split("\n");
+}
+
+function limpaPalavras(palavra) {
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+}
+
+function verificaPalavrasDuplicadas(texto) {
+  const listaPalavras = texto.split(" ");
+
+  const resultado = {};
+
+  listaPalavras.forEach((palavra) => {
+    const palavraLimpa = limpaPalavras(palavra);
+
+    if (palavraLimpa.length >= 3) {
+      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
+    }
+  });
+
+  return resultado;
+}
