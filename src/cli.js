94b8dd530ca04443bc6c12a8caa08df6884303f0
Victor <victor_oliveira93@outlook.com>
2025-06-12 18:25:39 -0300

    implementando o uso de Commander

--- src/cli.js
+++ src/cli.js
@@ -1,24 +1,60 @@
 import fs from "fs";
+import path from "path";
 import trataErros from "./erros/funcoesErros.js";
 import { contaPalavras } from "./index.js";
+import { montaSaidaArquivo } from "./helpers.js";
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .version("0.0.1")
+  .option("-t, --texto <string>", "caminho do texto a ser processado")
+  .option(
+    "-d, --destino <string>",
+    "caminho da pasta onde salvar o arquivo de resultados"
+  )
+  .action((options) => {
+    const { texto, destino } = options;
+
+    if (!texto || !destino) {
+      console.error("erro: favor inserir caminho de origem e destino");
+      program.help();
+      return;
+    }
+
+    const caminhoTexto = path.resolve(texto);
+    const caminhoDestino = path.resolve(destino);
+
+    try {
+      processaArquivo(caminhoTexto, caminhoDestino);
+      console.log("texto processado com sucesso");
+    } catch (erro) {
+      console.log("Ocorreu um erro no processamento ", erro);
+    }
+  });
+
+program.parse();
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 const endereco = caminhoArquivo[3];
 
-fs.readFile(link, "utf-8", (erro, texto) => {
-  try {
-    if (erro) throw erro;
-    const resultado = contaPalavras(texto);
-    criaESalvaArquivo(resultado, endereco);
-  } catch (erro) {
-    trataErros(erro);
-  }
-});
+function processaArquivo(texto, destino) {
+  fs.readFile(texto, "utf-8", (erro, texto) => {
+    try {
+      if (erro) throw erro;
+      const resultado = contaPalavras(texto);
+      criaESalvaArquivo(resultado, destino);
+    } catch (erro) {
+      trataErros(erro);
+    }
+  });
+}
 
 function criaESalvaArquivo(listaPalavras, endereco) {
   const arquivoNovo = `${endereco}/resultado.txt`;
-  const textoPalavras = JSON.stringify(listaPalavras);
+  const textoPalavras = montaSaidaArquivo(listaPalavras);
 
   fs.promises
     .writeFile(arquivoNovo, textoPalavras)
